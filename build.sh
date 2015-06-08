6baae7f7ea4b83189bbcde95476b42c64dff9c31
Robert Nelson <robertcnelson@gmail.com>
2015-06-08 11:25:50 -0500

    patch cleanup
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- build.sh
+++ build.sh
@@ -20,19 +20,18 @@ cd ${package_name}_${package_version}/
 git checkout ${git_sha} -b tmp
 
 git_apply="git apply"
-
-#git_apply="git am"
-
-echo ""
-echo "patches"
+#git_apply="git am --whitespace=fix"
 
 ${git_apply} ${DIR}/patches/0001-bb.org-big-revert.patch
 
-${git_apply} ${DIR}/patches/0001-bb.org-defaults.patch
+${git_apply} ${DIR}/patches/0002-bb.org-defaults.patch
 if [ "x${arch}" = "xarmv7l" ] ; then
-	${git_apply} ${DIR}/patches/0002-bb.org-use-systemd.patch
+	${git_apply} ${DIR}/patches/0003-bb.org-use-systemd.patch
 fi
-${git_apply} ${DIR}/patches/0003-core-dont-updateCore-we-checkout-a-sha-commit-and-do.patch
+${git_apply} ${DIR}/patches/0004-core-dont-updateCore-we-checkout-a-sha-commit-and-do.patch
+
+#git format-patch -4 -o ${DIR}/patches/
+#exit
 
 mkdir -p ~/.c9/
 echo 1 > ~/.c9/installed
