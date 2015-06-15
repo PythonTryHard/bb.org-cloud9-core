24dc41cba2be55aafefc5650b37dae3c3ad30748
Robert Nelson <robertcnelson@gmail.com>
2015-06-15 12:32:08 -0500

    pty revert is fixed now
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- build.sh
+++ build.sh
@@ -47,6 +47,16 @@ if [ "x${arch}" = "xarmv7l" ] ; then
 fi
 
 if [ ! "x${arch}" = "xarmv7l" ] ; then
+	project="nak"
+	echo ""
+	echo "Build: [npm install ${project} --arch=armhf]"
+	npm install ${project} --arch=armhf
+
+	project="pty.js@0.2.7-1"
+	echo ""
+	echo "Build: [npm install ${project} --arch=armhf]"
+	npm install ${project} --arch=armhf
+
 	node server.js -p 8181 -l 0.0.0.0 -a :
 else
 	rm -Rf build/standalone
