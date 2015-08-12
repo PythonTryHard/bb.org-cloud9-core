24849efb035dc63080d832e9cbfc3f103e10e5da
Robert Nelson <robertcnelson@gmail.com>
2015-08-12 10:16:09 -0500

    3.0.1+git20150811
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -4,12 +4,12 @@
 
 package_name="c9-core"
 debian_pkg_name="${package_name}"
-package_version="3.0.1+git20150715"
+package_version="3.0.1+git20150811"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="https://github.com/c9/core.git"
-git_sha="9ce99fb2f0fc45fecc5448b28301e3258835383e"
+git_sha="d0300319b89a87daf83c035be9e537307b6205a6"
 reprepro_dir="c/${package_name}"
 dl_path=""
 
