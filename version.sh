c940d980b51630d818f907d86576d398350fe10b
Robert Nelson <robertcnelson@gmail.com>
2016-11-14 14:18:31 -0600

    bisect: 49cb02527410890b2ff66524f526dfdfb288d832
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -9,7 +9,7 @@ package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="https://github.com/c9/core.git"
-git_sha="a4951f6a5e54525456a3c125972fbcb4ef4b3f24"
+git_sha="49cb02527410890b2ff66524f526dfdfb288d832"
 reprepro_dir="c/${package_name}"
 dl_path=""
 
@@ -19,7 +19,8 @@ dl_path=""
 #https://github.com/c9/core/commit/9e1bb472c6e671bba702dc8824526632f90af89d
 
 #20161114:
+#https://github.com/c9/core/compare/14f675858be5a639c2991a01ab46ed792dde6723...49cb02527410890b2ff66524f526dfdfb288d832
 #git bisect start
 #git bisect bad 32fc12bf922288d0c2b518c73aa75e19f14bed8b
 #git bisect good 14f675858be5a639c2991a01ab46ed792dde6723
-
+#git bisect bad 49cb02527410890b2ff66524f526dfdfb288d832
